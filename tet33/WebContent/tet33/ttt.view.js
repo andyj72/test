fc44059e9459b1f2f0429f1ba3053f14d2270baa
andy <andyj72@nate.com>
2018-12-12 15:23:38 +0900

    111

---
+++ tet33/WebContent/tet33/ttt.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("tet33.ttt", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf tet33.ttt
+	*/ 
+	getControllerName : function() {
+		return "tet33.ttt";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf tet33.ttt
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Title",
+			content: [
+			
+			]
+		});
+	}
+
+});
